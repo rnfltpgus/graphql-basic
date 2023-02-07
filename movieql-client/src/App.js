276fee9a1d71816348341e944e60355d1e200c3e
rnfltpgus <jpp901220@gmail.com>
2023-02-07 23:42:18 +0900

    🔩 Feat, Style: css 적용, 화면 구현

--- movieql-client/src/App.js
+++ movieql-client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Movie from './routes/Movie';
 import Movies from './routes/Movies';
 
--- movieql-client/src/index.js
+++ movieql-client/src/index.js
@@ -1,6 +1,8 @@
-import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { ApolloProvider } from '@apollo/client';
+
 import App from './App';
 import client from './client';
 
